2c0dcff210e39abf99efd431ed7e2296df85278e
LVedantam <LAKSHMIL3547@GMAIL.COM>
2025-04-07 09:36:50 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-import requests
+
 
 # Write directly to the app
 st.title("Customize your Smoothie! :cup_with_straw:")
@@ -13,15 +13,8 @@ st.write('The name on the order will be: ',name_on_order)
 
 cnx=st.connection("snowflake")
 session = cnx.session()
-
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
-#st.stop()
-
-#convert the Dataframe to a Pandas Dataframe so we can use the LOC function
-pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
 
 
 ingredients_list = st.multiselect(
@@ -35,14 +28,7 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-        
-        st.subheader(fruit_chosen + 'Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.SMOOTHIEFROOT.com/api/fruit/watermelon")
-        st.text(smoothiefroot_response)
-   
+
     #st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(name_on_order,ingredients)
@@ -60,6 +46,12 @@ if ingredients_list:
     if name_on_order:
         st.success(f'Your smoothie is ordered, {name_on_order}!', icon="✅")
         
+
+
+
+
+
+
 
 
 
