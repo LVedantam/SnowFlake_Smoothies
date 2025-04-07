916eecb522d902edd07e0e23c7919188e32ddeca
LVedantam <LAKSHMIL3547@GMAIL.COM>
2025-04-07 09:59:12 -0400

    Update streamlit_app.py
    new section to display nutrition information

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,10 @@ if ingredients_list:
 
     if name_on_order:
         st.success(f'Your smoothie is ordered, {name_on_order}!', icon="✅")
-        
+      #New section to display smoothiefroot nutrition information
+    import requests
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    st.text(smoothiefroot_response)
 
 
 
